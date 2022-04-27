54a7a611d3d0e9e408afa7d69a996b7e61d87212
Veronika <voronovaveronika@gmail.com>
2022-04-27 11:54:08 +0100

    Remove dist folder and add src

--- dir/utils/digest.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.convertStringToDigest = void 0;
-const crypto_1 = require("crypto");
-const convertStringToDigest = (string, algorithm = "sha256") => (0, crypto_1.createHash)(algorithm).update(string).digest();
-exports.convertStringToDigest = convertStringToDigest;
